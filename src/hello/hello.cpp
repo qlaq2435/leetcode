c9b585b5a9d4870ece86fb600e3d3ed4d159a72a
qlaq2435 <qlaq2435@outlook.com>
2023-05-15 11:25:36 +0800

    取消模板文件

--- src/hello/hello.cpp
+++
@@ -1,9 +0,0 @@
-#include "hello.h"
-
-#include <iostream>
-
-using std::cout, std::endl;
-void sayhello() {
-  cout << "hello" << endl;
-  return;
-}
--- src/main.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-#include "hello.h"
-
-int main(int /*argc*/, char* /*argv*/[]) {
-  sayhello();
-  return 0;
-}
